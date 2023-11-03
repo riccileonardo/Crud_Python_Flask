0775dabf4f62dc28f32ef63f29c004fe4cd34683
riccileonardo <122654112+riccileonardo@users.noreply.github.com>
2023-11-02 22:18:29 -0400

    update

--- cadastro.js
+++ cadastro.js
@@ -1,12 +1,14 @@
-document.getElementById("marca").addEventListener("change", function() {
-  var campoOutro = document.getElementById("campo-outro");
-  var outraMarcaInput = document.getElementById("outra-marca");
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("marca").addEventListener("change", function() {
+    var campoOutro = document.getElementById("campo-outro");
+    var outraMarcaInput = document.getElementById("outra-marca");
 
-  if (this.value === "outro") {
-    campoOutro.style.display = "block";
-    outraMarcaInput.setAttribute("required", "true");
-  } else {
-    campoOutro.style.display = "none";
-    outraMarcaInput.removeAttribute("required");
-  }
+    if (this.value === "outro") {
+      campoOutro.style.display = "block";
+      outraMarcaInput.setAttribute("required", "true");
+    } else {
+      campoOutro.style.display = "none";
+      outraMarcaInput.removeAttribute("required");
+    }
+  });
 });
