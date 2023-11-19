d3a301320b5a2ec6fe2e79a60091b135cd3c9e64
riccileonardo <122654112+riccileonardo@users.noreply.github.com>
2023-11-19 17:39:38 -0400

    Ajuste

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, Response, flash
 import mysql.connector # Biblioteca para conexão  e consulta do banco MySQL
+from datetime import datetime # Biblioteca para trabalhar com datas
 import hashlib # Biblioteca para criptografar senha
+import csv
+import io  # Módulo io para trabalhar com dados binários
+import pdb # Biblioteca para debugar o código
 
 app = Flask(__name__)
 app.secret_key = '123123'
@@ -46,6 +50,7 @@ def login():
         user = cursor.fetchone() #retorna o usuario e a senha
         if user:
             session['username'] = email # ------------------------> Se o usuario existir, ele vai criar uma sessão com o username
+            flash('Logado com Sucesso!', 'success')
             return redirect('/') # --------------------------------> Redireciona para a pagina inicial
         else:
             return "Login inválido. <a href='/login'>Tente novamente</a>"
@@ -90,6 +95,7 @@ def veiculos(): #--------------------------------------------------> Corrigido p
 def cadastro():
     if 'username' in session:
         if request.method == 'POST':
+            data_atual = datetime.now().strftime('%Y-%m-%d')
             id_usuario = get_user_id(str(session['username']))
             ano = request.form['ano']
             placa = request.form['placa']
@@ -101,10 +107,11 @@ def cadastro():
             id_classificacao = request.form['id_classificacao']
             id_tipo = request.form['id_tipo']
             cursor.execute("""
-            INSERT INTO veiculo (ano, placa, modelo, quilometragem, cor, id_marca, id_transmissao, id_classificacao, id_tipo, id_usuario)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, (ano, placa, modelo, quilometragem, cor, id_marca, id_transmissao, id_classificacao, id_tipo, id_usuario))
+            INSERT INTO veiculo (ano, placa, modelo, quilometragem, cor, id_marca, id_transmissao, id_classificacao, id_tipo, id_usuario, data_criacao)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (ano, placa, modelo, quilometragem, cor, id_marca, id_transmissao, id_classificacao, id_tipo, id_usuario, data_atual))
             db.commit()
+            flash('Veículo cadastrado com sucesso!', 'success')
             return redirect('/veiculos')
         
         cursor.execute("SELECT id_transmissao, nome from transmissao")
@@ -118,24 +125,13 @@ def cadastro():
         return render_template('/cadastro.html', transmissoes=transmissao, classificacoes=classificacao, tipos=tipo, marcas=marca)
     else:
         return redirect('/login')
-
-
-@app.route('/relatorios', methods=['GET', 'POST'])
-def relatorio():
-    if 'username' in session:
-        if request.method == 'POST':
-            data_inicio = request.form['data_inicio']
-            data_fim = request.form['data_fim']
-            cursor.execute("SELECT * FROM aluguel WHERE data_inicio BETWEEN %s AND %s", (data_inicio, data_fim))
-            relatorios = cursor.fetchall()
-            return render_template('relatorios.html', relatorios=relatorios)
-        return render_template('relatorios.html')
     
 @app.route('/veiculos/<id_veiculo>/delete', methods=['POST'])
 def excluir(id_veiculo):
     if 'username' in session:
         cursor.execute("DELETE FROM veiculo WHERE id_veiculo = %s", (id_veiculo,))
         db.commit()
+        flash('Veículo excluído com sucesso!', 'success')
         return redirect('/veiculos')
 
 @app.route('/veiculos/<id_veiculo>/editar', methods=['GET', 'POST'])
@@ -159,6 +155,7 @@ def editar(id_veiculo):
                 """, (ano, placa, modelo, quilometragem, cor, id_marca, id_transmissao, id_classificacao, id_tipo, id_veiculo)
             )
             db.commit()
+            flash('Veículo editado com sucesso!', 'success')
             return redirect('/veiculos')
         
         cursor.execute("SELECT * FROM veiculo WHERE id_veiculo = %s", (id_veiculo,))
@@ -173,6 +170,122 @@ def editar(id_veiculo):
         marca = cursor.fetchall()
         return render_template('editar.html', transmissoes=transmissao, classificacoes=classificacao, tipos=tipo, marcas=marca, veiculo=veiculo)
 
+@app.route('/relatorios', methods=['GET'])
+def relatorio():
+    if 'username' in session:
+        cursor.execute("SELECT id_tipo, nome from tipo")
+        tipo = cursor.fetchall()
+        cursor.execute("SELECT id_marca, nome from marca")
+        marca = cursor.fetchall()
+        return render_template('relatorios.html', tipos=tipo, marcas=marca)
+
+@app.route('/relatorios/marca', methods=['GET'])
+def relatorio_marca():
+    if 'username' in session:
+        id_marca = request.args.get("marca")
+        data_inicial = request.args.get("data_inicial")
+        data_final = request.args.get("data_final")
+        cursor.execute("""
+            SELECT
+            veiculo.data_criacao AS data_veiculo,
+            veiculo.id_marca AS id_marca,
+                veiculo.id_veiculo AS id_veiculo,
+                usuario.nome AS nome_usuario,
+                marca.nome AS marca_veiculo,
+                veiculo.ano AS ano_veiculo,
+                veiculo.placa AS placa_veiculo,
+                veiculo.modelo AS modelo_veiculo,
+                veiculo.quilometragem AS quilometragem_veiculo,
+                veiculo.cor AS cor_veiculo,
+                transmissao.nome AS transmissao_veiculo,
+                classificacao.nome AS classificacao_veiculo,
+                tipo.nome AS tipo_do_veiculo
+            FROM
+                veiculo
+                JOIN usuario ON veiculo.id_usuario = usuario.id_usuario
+                JOIN marca ON veiculo.id_marca = marca.id_marca
+                JOIN transmissao ON veiculo.id_transmissao = transmissao.id_transmissao
+                JOIN classificacao ON veiculo.id_classificacao = classificacao.id_classificacao
+                JOIN tipo ON veiculo.id_tipo = tipo.id_tipo
+            WHERE
+                veiculo.data_criacao BETWEEN %s AND %s
+                AND veiculo.id_marca = %s
+            """, (data_inicial, data_final, id_marca)
+            )
+        relatorios = cursor.fetchall()
+        # Use a biblioteca csv para gerar dados CSV
+        csv_data = io.StringIO()
+        csv_writer = csv.writer(csv_data)
+        
+        # Escreva o cabeçalho
+        header = [desc[0] for desc in cursor.description]
+        csv_writer.writerow(header)
+        
+        # Escreva os dados
+        csv_writer.writerows(relatorios)
+
+        # Crie a resposta Flask
+        response = Response(
+            csv_data.getvalue(),
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=relatorio_veiculos.csv'}
+        )
+
+        return response
+
+@app.route('/relatorios/tipo', methods=['GET'])
+def relatorio_tipo():
+    if 'username' in session:
+        id_tipo = request.args.get('id_tipo')
+        data_inicial = request.args.get('data_inicial')
+        data_final = request.args.get('data_final')
+        cursor.execute("""
+            SELECT
+            veiculo.data_criacao AS data_veiculo,
+            veiculo.id_marca AS id_marca,
+                veiculo.id_veiculo AS id_veiculo,
+                usuario.nome AS nome_usuario,
+                marca.nome AS marca_veiculo,
+                veiculo.ano AS ano_veiculo,
+                veiculo.placa AS placa_veiculo,
+                veiculo.modelo AS modelo_veiculo,
+                veiculo.quilometragem AS quilometragem_veiculo,
+                veiculo.cor AS cor_veiculo,
+                transmissao.nome AS transmissao_veiculo,
+                classificacao.nome AS classificacao_veiculo,
+                tipo.nome AS tipo_do_veiculo
+            FROM
+                veiculo
+                JOIN usuario ON veiculo.id_usuario = usuario.id_usuario
+                JOIN marca ON veiculo.id_marca = marca.id_marca
+                JOIN transmissao ON veiculo.id_transmissao = transmissao.id_transmissao
+                JOIN classificacao ON veiculo.id_classificacao = classificacao.id_classificacao
+                JOIN tipo ON veiculo.id_tipo = tipo.id_tipo
+            WHERE
+                veiculo.data_criacao BETWEEN %s AND %s
+                AND veiculo.id_tipo = %s
+            """, (data_inicial, data_final, id_tipo)
+            )
+        relatorios = cursor.fetchall()
+        # Use a biblioteca csv para gerar dados CSV
+        csv_data = io.StringIO()
+        csv_writer = csv.writer(csv_data)
+        
+        # Escreva o cabeçalho
+        header = [desc[0] for desc in cursor.description]
+        csv_writer.writerow(header)
+        
+        # Escreva os dados
+        csv_writer.writerows(relatorios)
+
+        # Crie a resposta Flask
+        response = Response(
+            csv_data.getvalue(),
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=relatorio_veiculos.csv'}
+        )
+
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
